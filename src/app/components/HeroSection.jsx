c4c866547432e5ecb1c8057497b097d39b460f11
Parmeet Kaur <kparmeet300@gmail.com>
2025-08-01 17:10:12 +0530

    added resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
                 Hire Me
               </Link>
               <Link
-                href="/"
+                href="/Parmeet_kaur_resume.pdf"
                 className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white text-center"
               >
                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
